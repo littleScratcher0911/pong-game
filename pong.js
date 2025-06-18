ca75c5f87cc01ef28c45fe5537f280991644e60b
littleScratcher0911 <luke.rink@icloud.com>
2025-06-18 18:44:01 +0200

    Update pong.js

--- pong.js
+++ pong.js
@@ -13,3 +13,25 @@ function sendMove(direction) {
   const moveData = JSON.stringify({ type: 'move', direction });
   socket.send(moveData);
 }
+
+const canvas = document.getElementById('gameCanvas');
+
+canvas.addEventListener('touchstart', handleTouchStart);
+canvas.addEventListener('touchmove', handleTouchMove);
+canvas.addEventListener('touchend', handleTouchEnd);
+
+let touchStartY = 0;
+
+function handleTouchStart(event) {
+  touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+  const touchY = event.touches[0].clientY;
+  const deltaY = touchY - touchStartY;
+  // Bewege das Paddle basierend auf deltaY
+}
+
+function handleTouchEnd() {
+  // Beende die Bewegung
+}
